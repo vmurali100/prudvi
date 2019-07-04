21dc7b3fa6d94639f68ed6e1a4e87da58ce86eb2
vmurali100 <vmurali100@gmail.com>
2019-07-04 17:07:48 +0530

    Arrays and Its Methods

---
+++ arrays.py
@@ -0,0 +1,18 @@
+users = ["Murali","Krishna","Ram","Murali"]
+
+# users.append("Ravi")
+# print(users)
+#
+# users1= users.copy()
+# print(users1)
+# print(users.count("Murali"))
+# usersName=["RAVI","Somu"]
+# users.extend(usersName)
+# print(users)
+# print(users.index("Murali"))
+# users.insert(2,"VMR")
+# users.pop(1)
+users.remove("Murali")
+users.reverse()
+users.sort()
+print(users)
--- lamda.py
+++ lamda.py
@@ -1,3 +1,14 @@
-some = lambda a: a+10;
+# some = lambda a: a+10;
 
-print(some(20))
+# print(some(20))
+#
+# some2 = lambda a,b,c: a+b+c
+#
+# print(some2(20,40,30))
+
+def calculate(a):
+    return  lambda b: b*a
+
+calc = calculate(10)
+
+print(calc(20))
